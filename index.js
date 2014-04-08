129c4da3ee90fd8c7b4c92546b7f2f229342634c
Jimmy Hillis <jimmy@hillis.me>
2014-04-08 22:56:11 +0800

    Setup templates with jade-lang and render two basic test routes

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 var express = require('express')
   , app = express();
 
+// Server configuration
+app.set('views', 'templates');
+app.set('view engine', 'jade');
+app.engine('jade', require('jade').__express);
+
 var routes = require('./routes');
 
-app.get('/', function(req, res) {
-    res.send('TAP THAT!');
+// HTML routes
+app.get('/', function (req, res) {
+    res.render('index');
+});
+app.get('/map', function (req, res) {
+    res.render('map');
 });
 
+// JSON routes
 app.get('/pubs.json', routes.pub.all);
 
 app.listen(3000);
