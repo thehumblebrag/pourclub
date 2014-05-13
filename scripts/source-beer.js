e567cf008ae782042f2d0766a48851498a83b174
Jimmy Hillis <jimmy@hillis.me>
2014-05-13 23:02:28 +0800

    Clean up and end the script nicely

--- scripts/source-beer.js
+++ scripts/source-beer.js
@@ -1,37 +1,36 @@
 var url = require('url');
 var request = require('request');
 var _ = require('underscore');
-var apiKey = 'API_KEY_HERE';
-var apiUrl = 'api.brewerydb.com/';
+var api_key = 'KEY_HERE';
+var api_url = 'api.brewerydb.com';
 
 var Booze = require('../models/booze');
 var Creator = require('../models/creator');
 
 function getUrl(method, params) {
     var apiParams = {
-        key: apiKey,
+        key: api_key,
         format: 'json'
     };
-
     return url.format({
         protocol: 'https',
-        hostname: apiUrl,
+        hostname: api_url,
         pathname: 'v2/' + method,
         query: _.extend(apiParams, params)
     });
 }
 
-function handleBeers(beerList) {
-    beerList.forEach(function (beer) {
+function handleBeers(beers) {
+    beers.forEach(function (beer) {
         console.log('Saving beer: ' + beer.name);
-
-        (new Booze({
+        var new_beer = new Booze({
             name: beer.name,
             style: beer.style && beer.style.name,
             description: beer.description,
             abv: beer.abv,
             creator_name: beer.breweries && beer.breweries[0].name
-        })).save(function (err) {
+        });
+        new_beer.save(function (err) {
             if (err) {
                 console.log(err);
             }
@@ -40,18 +39,22 @@ function handleBeers(beerList) {
 }
 
 function getPageOfBeer(pageNumber) {
-    request({ url: getUrl('beers', { p: pageNumber }), json: true },
-        function (error, response, body) {
-            handleBeers(body.data);
-        });
+    request({ url: getUrl('beers', { p: pageNumber }), json: true }, function (error, response, body) {
+        handleBeers(body.data);
+    });
 }
 
-request({ url: getUrl('beers', {withBreweries: 'Y'}), json: true}, function (error, response, body) {
+request({ url: getUrl('beers', { withBreweries: 'Y' }), json: true}, function (error, response, body) {
     // Do this to get the total pages of beer.
-    if (!error && response.statusCode == 200) {
+    if (error) {
+        console.error(error);
+    }
+    else if (body.status == 'failure') {
+        console.error(body.errorMessage);
+    }
+    else if (response.statusCode == 200) {
         // This is the first beer list, so handle it anyway.
         handleBeers(body.data);
-
         // Now get the rest.
         for (var i = 2; i < body.numberOfPages; i++) {
             getPageOfBeer(i);
