f7f79e32a1c0e71dfab55bf2123a7b9b8b4e3074
Jimmy Hillis <jimmy@hillis.me>
2014-06-11 20:11:01 +0800

    Refactor for code style and make some logic fixes

--- bin/associate-drink-to-location.js
+++ bin/associate-drink-to-location.js
@@ -3,15 +3,23 @@ var booze = require('../lib/booze');
 var untappd = require('../lib/untappd');
 var Pub = require('../models/pub');
 
-Pub.find(function(err, pubs) {
+Pub.find().limit(5).exec(function(err, pubs) {
     async.each(pubs, function (pub, done) {
-        untappd.findBeersAtFoursquareLocation("50488cc7e4b0d6e661d3ebc1", function (err, beers) {
-            if (err) { console.error(err); process.exit(1); }
+        untappd.findBeersAtFoursquareLocation(pub.foursquare_id, function (err, beers) {
+            if (err) {
+                console.error(err);
+                process.exit(1);
+            }
             async.map(beers, function (beer, done) {
                 booze.searchOne(beer.beer_name, done);
-            }, function (err, beers) {
-                if (err) return done(err);
-                if (!boozes) done();
+            },
+            function (err, beers) {
+                if (err) {
+                    return done(err);
+                }
+                if (!beers) {
+                    return done();
+                }
                 pub.boozes = beers;
                 pub.save(done);
             });
