5432ce6664c38f6cc1804d81547d8f8a2cea4481
Jimmy Hillis <jimmy@hillis.me>
2014-06-16 22:50:27 +0800

    Update script to use less API calls if the 4sq wasn't found

--- bin/associate-drink-to-location.js
+++ bin/associate-drink-to-location.js
@@ -3,12 +3,15 @@ var booze = require('../lib/booze');
 var untappd = require('../lib/untappd');
 var Pub = require('../models/pub');
 
-Pub.find().limit(5).exec(function(err, pubs) {
+Pub.find().limit(10).exec(function(err, pubs) {
     async.each(pubs, function (pub, done) {
-        untappd.findBeersAtFoursquareLocation(pub.foursquare_id, function (err, beers) {
+        untappd.findBeersAtLocation(pub, function (err, beers) {
             if (err) {
                 console.error(err);
                 process.exit(1);
+            }
+            if (!beers) {
+                done();
             }
             async.map(beers, function (beer, done) {
                 booze.searchOne(beer.beer_name, done);
@@ -24,5 +27,12 @@ Pub.find().limit(5).exec(function(err, pubs) {
                 pub.save(done);
             });
         });
+    }, function (err) {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        console.log('Done');
+        process.exit();
     });
 });
